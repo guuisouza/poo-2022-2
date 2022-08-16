8e49d145ca95ab656b0b6731554d888a56b46929
Guilherme Souza <89078191+guuisouza@users.noreply.github.com>
2022-08-16 11:05:36 -0300

    Add files via upload

---
+++ Projeto1/src/main/java/br/edu/fatecfranca/projeto1/Teste.java
@@ -0,0 +1,27 @@
+package br.edu.fatecfranca.projeto1;
+
+public class Teste {
+
+    public static void main(String[] args) {
+      //vamos criar um objeto da classe produto
+      //estamos instanciando a classe Produto
+      //ou criando uma instância da classe produto
+      Produto obj1 = new Produto();
+      obj1.nome = "Bola de basquete";
+      obj1.qtde = 3;
+      obj1.preco = 90.40;
+      obj1.mostrar();
+      obj1.comprar(2);
+      obj1.vender(1);
+      obj1.mostrar();
+      
+      Produto obj2 = new Produto ("Máscara para Covid-19", 5, 5.20); //chama o metodo construtor
+      obj2.mostrar();
+      obj2.comprar(2);
+      obj2.subir(3);
+      obj2.mostrar();
+      
+      Produto obj3 = new Produto ("Almoço", 2, 12.90); //chama o metodo construtor
+      obj3.mostrar();
+    }
+}
