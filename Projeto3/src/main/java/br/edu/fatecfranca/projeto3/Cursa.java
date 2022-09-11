8a3ae25e7fe24363b641fd55961d2905ed8214e2
Guilherme Souza <89078191+guuisouza@users.noreply.github.com>
2022-09-11 17:21:24 -0300

    Add files via upload

---
+++ Projeto3/src/main/java/br/edu/fatecfranca/projeto3/Cursa.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.fatecfranca.projeto3;
+import java.util.Date;
+
+/**
+ *
+ * @author pc gaming
+ */
+public final class Cursa {
+    private int codigo;
+    private Date data;
+    // agregação - criamos um objeto-parte dentro do objeto-todo
+    private Aluno aluno; // referências para um objeto aluno
+    // agregação - criamos um objeto-parte dentro do objeto-todo
+    private Disciplina disciplina; // referências para um objeto aluno
+    
+    public Cursa() {
+        //aluno.getNome();
+        //aluno.getRa();
+        //disciplina.getId();
+        //disciplina.getSigla();
+    }
+
+    public Cursa(int codigo, Date data, Aluno aluno, Disciplina disciplina) {
+        this.setCodigo(codigo);
+        this.setData(data);
+        this.setAluno(aluno);
+        this.setDisciplina(disciplina);
+    }
+
+    public void setDisciplina(Disciplina disciplina) {
+        this.disciplina = disciplina;
+    }
+
+    public Disciplina getDisciplina() {
+        return disciplina;
+    }
+
+    
+    
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public Aluno getAluno() {
+        return aluno;
+    }
+
+    public void setAluno(Aluno aluno) {
+        this.aluno = aluno;
+    }
+
+    @Override
+    public String toString() {
+        return "Cursa{" + "codigo=" + codigo + ", data=" + data + 
+                ", \n aluno=" + aluno + 
+                ", \n disciplina=" + disciplina + '}';
+    }
+}
