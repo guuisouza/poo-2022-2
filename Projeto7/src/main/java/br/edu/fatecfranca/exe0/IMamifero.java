52c4ad2663de0744e786392ef73e475cc5f55874
Guilherme Souza <89078191+guuisouza@users.noreply.github.com>
2022-10-04 10:57:41 -0300

    Add files via upload

---
+++ Projeto7/src/main/java/br/edu/fatecfranca/exe0/IMamifero.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package br.edu.fatecfranca.exe0;
+
+/**
+ *
+ * @author 1090482111037
+ */
+public interface IMamifero {
+    //todos os métodos são abstratos
+    public void darMama();
+}
